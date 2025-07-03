207fe59b9996b9c149d545a0bc221348ec268e83
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-07-03 03:15:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -479,5 +479,5 @@ def caja():
 
 # ---------- MAIN ----------
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False)
+
